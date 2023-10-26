fa311b3445a2ce5ce2a96a6f338e9b292cf40d43
Galela-Nayar <galelanayar05@gmail.com>
2023-10-26 21:03:25 +0200

    pour l'instant tester les requetes de la bdd avec curl

--- cp/src/main/java/backend/cp/controller/ProjetController.java
+++ cp/src/main/java/backend/cp/controller/ProjetController.java
@@ -2,13 +2,11 @@ package backend.cp.controller;
 
 import backend.cp.modele.Projet;
 import backend.cp.repository.ProjetRepository;
-import backend.cp.service.ProjetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
-import java.util.List;
 
 @RestController
 @RequestMapping("/projets")
--- cp_frontend/src/app/app.component.ts
+++ cp_frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cp_frontend';
+
+    user = {
+      nom: '',
+      prenom: '',
+      user_name: '',
+      mail: '',
+      miniature: '',
+      bio: ''
+    };
+
+  constructor(private http: HttpClient) {}
+
+  createUtilisateur() {
+    console.log("1111111111");
+    const headers = { 'Content-Type': 'application/json' };
+    console.log("222222222222");
+
+    this.http.post('http://localhost:8080/utilisateurs/create', this.user, { headers }).subscribe(
+      (response) => {
+        console.log('Utilisateur créé avec succès', response);
+      },
+      (error) => {
+        console.error('Erreur lors de la création de l\'utilisateur', error);
+      }
+    );
+    console.log("333333333");
+  }
 }
+  
+
