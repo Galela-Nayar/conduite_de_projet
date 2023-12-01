aecdb078fa4a81c79a7a63e07c7d3cf5bf4c0480
ngalela01 <149369383+ngalela01@users.noreply.github.com>
2023-12-01 23:02:24 +0100

    ok pour scrum

--- cp_frontend/src/app/user-home-page/project/section-scrum/section-scrum.component.ts
+++ cp_frontend/src/app/user-home-page/project/section-scrum/section-scrum.component.ts
@@ -1,5 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewChecked, ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+  AfterViewChecked,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs';
@@ -7,14 +14,18 @@ import { ObservableService } from 'src/app/observable/observable-projet.service'
 import Section from 'src/interface/Section';
 import { SectionService } from '../section/section.service';
 import { CreateTaskComponent } from '../../create-task/create-task.component';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-section-scrum',
   templateUrl: './section-scrum.component.html',
-  styleUrls: ['./section-scrum.component.css']
+  styleUrls: ['./section-scrum.component.css'],
 })
-export class SectionScrumComponent implements AfterViewChecked{
+export class SectionScrumComponent implements AfterViewChecked {
   @ViewChild('textareaNom') textareaNom!: ElementRef;
   @Input() id!: String | null;
   @Input() projetId!: String;
@@ -56,9 +67,14 @@ export class SectionScrumComponent implements AfterViewChecked{
               this.tasks = this.section.taches;
             });
         });
-        //Les droits de l'utilisateur actuel
-        this.http.get(`http://localhost:8080/projets/droitUtilisateur?idUtilisateur=${this.id}&idProjet=${this.projetId}`, {responseType: 'text'}).subscribe((data: string) => {
-              this.droitUtilisateurActuel = data;
+      //Les droits de l'utilisateur actuel
+      this.http
+        .get(
+          `http://localhost:8080/projets/droitUtilisateur?idUtilisateur=${this.id}&idProjet=${this.projetId}`,
+          { responseType: 'text' }
+        )
+        .subscribe((data: string) => {
+          this.droitUtilisateurActuel = data;
         });
     }
   }
@@ -73,45 +89,48 @@ export class SectionScrumComponent implements AfterViewChecked{
   adjustTextareaNom(event?: any) {
     let target = event ? event.target : this.textareaNom.nativeElement;
     target.style.height = 'auto';
-    target.style.height = (target.scrollHeight) + 'px';
-}
+    target.style.height = target.scrollHeight + 'px';
+  }
 
   updateTaskNom() {
-    this.http.put(`http://localhost:8080/sections/updateNom?id=${this.sectionId}&nom=${this.section.nom}`, {responseType:"text"}).
-    subscribe((tacheData) => {
-      this.updateSection();
-      this.isEditingNom = false;
-      this.observerService.notifyTask();
-      this.cd.detectChanges()
-    })
+    this.http
+      .put(
+        `http://localhost:8080/sections/updateNom?id=${this.sectionId}&nom=${this.section.nom}`,
+        { responseType: 'text' }
+      )
+      .subscribe((tacheData) => {
+        this.updateSection();
+        this.isEditingNom = false;
+        this.observerService.notifyTask();
+        this.cd.detectChanges();
+      });
   }
 
   updateSection() {
     this.taskSubscription = this.observableService
-        .getObservableTask()
-        .subscribe((response) => {
-          this.http
-            .get<Section>(
-              `http://localhost:8080/sections/section?id=${this.sectionId}`
-            )
-            .subscribe((response) => {
-              this.section = response;
-              this.tasks = this.section.taches;
-            });
-        });
+      .getObservableTask()
+      .subscribe((response) => {
+        this.http
+          .get<Section>(
+            `http://localhost:8080/sections/section?id=${this.sectionId}`
+          )
+          .subscribe((response) => {
+            this.section = response;
+            this.tasks = this.section.taches;
+          });
+      });
   }
-  
-editNom(){
-  if(this.droitUtilisateurActuel != 'Visiteur') this.isEditingNom = true
-}
+
+  editNom() {
+    if (this.droitUtilisateurActuel != 'Visiteur') this.isEditingNom = true;
+  }
 
   onPlusClick(event: MouseEvent): void {
     const dialogRef = this.dialog.open(CreateTaskComponent, {
       data: this.sectionId,
     });
 
-    dialogRef.afterClosed().subscribe((result) => {
-    });
+    dialogRef.afterClosed().subscribe((result) => {});
   }
 
   onSettingClick(event: MouseEvent) {
@@ -122,15 +141,56 @@ editNom(){
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
-    this.http
-      .put(
-        `http://localhost:8080/sections/updateTaches?id=${this.sectionId}&taches=${this.tasks}`,
-        {}
-      )
-      .subscribe((response) => {
-        this.observableService.notifySection();
-      });
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+      this.http
+        .put(
+          `http://localhost:8080/sections/updateTaches?id=${this.sectionId}&taches=${this.tasks}`,
+          {}
+        )
+        .subscribe((response) => {
+          this.observableService.notifySection();
+        });
+    } else {
+      const movedTaskId = event.previousContainer.data[event.previousIndex];
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      this.moveTaskbetweenSection(movedTaskId);
+    }
   }
 
+  moveTaskbetweenSection(taskId: string) {
+    this.http
+      .get(
+        `http://localhost:8080/sections/searchSectionofTask?idTache=${taskId}`,
+        { responseType: 'text' }
+      )
+      .subscribe((response: String) => {
+        if (response != 'pas trouvé') {
+          this.http
+            .get(
+              `http://localhost:8080/sections/removeTache?id=${response}&tacheId=${taskId}`,
+              { responseType: 'text' }
+            )
+            .subscribe((response2: String) => {
+              console.log('supprimer tache dans section: ' + response2);
+
+              this.http
+                .get(
+                  `http://localhost:8080/sections/add-tache?sectionId=${this.sectionId}&tacheId=${taskId}`,
+                  { responseType: 'text' }
+                )
+                .subscribe((response) => {});
+
+              this.observableService.notifyTask();
+            });
+        } else {
+          console.log("probleme d'idTache", response);
+        }
+      });
+  }
 }
